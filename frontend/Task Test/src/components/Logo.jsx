a51cc44c4bc899b8f17004ec0b1e9231e3857668
Hamas Ahmed <hamasahmed0@gmail.com>
2024-07-14 03:28:46 +0300

    feat: add company logo

---
+++ frontend/Task Test/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import companyLogo from '../assets/Sigma Software logo.png'
+
+const Logo = () => {
+  return (
+    <div className='w-1/2 mx-auto my-10'>
+      <img src={companyLogo} alt="Sigma Software logo" />
+    </div>
+  )
+}
+
+export default Logo
