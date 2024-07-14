22317c3dc04d32e3a00c00739a895b83cd006d4c
Hamas Ahmed <hamasahmed0@gmail.com>
2024-07-14 03:33:04 +0300

    feat: UI integration

--- frontend/Task Test/src/App.jsx
+++ frontend/Task Test/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Logo from './components/Logo'
+import CandidateInfo from './components/CandidateInfo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+      <Logo />
+      <CandidateInfo />
+    </>
   )
 }
 
