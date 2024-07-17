bd210092b78410fea5b9aec7899800f9583cd713
Hamas Ahmed <hamasahmed0@gmail.com>
2024-07-17 03:37:49 +0300

    fix: fix insert and update query

--- backend/model/candidates.js
+++ backend/model/candidates.js
@@ -6,12 +6,12 @@ const addCandidate = async (firstName, lastName, phoneNumber, email, timeInterva
   if (rows.length > 0) {
     return await db.execute(
       'UPDATE `job_candidates` SET first_name = ?, last_name = ?, phone_number = ?, linkedin_profile_URL = ?, github_profile_URL = ?, comment = ?, start_time = ?, end_time = ? WHERE email = ?',
-      [firstName, lastName, phoneNumber, linkedInURL, GitHubURL, comment, timeInterval[0].$H, timeInterval[1].$H, email]
+      [firstName, lastName, phoneNumber, linkedInURL, GitHubURL, comment, timeInterval[0], timeInterval[1], email]
     )
   } else {
     return await db.execute(
-      'INSERT INTO `job_candidates` (`candidate_id`, `first_name`, `last_name`, `phone_number`, `email`, `start_time`, `end_time`, `linkedin_profile_URL`, `github_profile_URL`, `comment`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)',
-      [firstName, lastName, phoneNumber, email,timeInterval[0].$H, timeInterval[1].$H, linkedInURL, GitHubURL, comment]
+      'INSERT INTO `job_candidates` (`candidate_id`, `first_name`, `last_name`, `phone_number`, `email`, `start_time`, `end_time`, `linkedin_profile_URL`, `github_profile_URL`, `comment`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [firstName, lastName, phoneNumber, email, timeInterval[0], timeInterval[1], linkedInURL, GitHubURL, comment]
     )
   }
 
